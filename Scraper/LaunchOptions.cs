cddfd127a74f76a17e201f94e5fd57152c87203c
Alexander Gimranov <alexander.gimranov@gmail.com>
2020-12-16 16:58:22 +0300

    developed app according to requirements

---
+++ Scraper/LaunchOptions.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace Scraper
+{
+	public class LaunchOptions
+    {
+		[Option('F', "file", Required = true, HelpText = "File name(s).", Separator = ',')]
+		public IEnumerable<string> FileNames { get; set; }
+
+		[Option('S', "strowords", Required = true, HelpText = "Words that will not be counted.", Separator = ',')]
+		public IEnumerable<string> StropWords { get; set; }
+
+		[Option('C', "characters", Required = true, HelpText = "Enable count the number of characters in the file?.")]
+		public bool CountCharacters { get; set; }
+
+		[Option('L', "captial", Required = false, Default = false, HelpText = "Enable count words that start with a Capital letter.")]
+		public bool CountCapltialLetter { get; set; }
+    }
+}
---
+++ Scraper/Program.cs
@@ -0,0 +1,79 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    partial class Program
+    {
+        public record FileProcessingResult(string FileName, int? CountCharacters, int? CountCapltialLetter, Exception Exception);
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<LaunchOptions>(args)
+                .WithParsedAsync(RunOptionsAsync)
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        static async Task RunOptionsAsync(LaunchOptions opts)
+        {
+            var tasks = opts.FileNames.Select(fn => ProcessingFile(fn, opts.StropWords, opts.CountCharacters, opts.CountCapltialLetter));
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("Processing result(s): ");
+            foreach(var task in tasks)
+            {
+                if (task.Result.Exception != null)
+                {
+                    Console.WriteLine($"'{task.Result.FileName}' - error : '{task.Result.Exception.Message}'");
+                }
+                else
+                {
+                    Console.WriteLine(task.Result.FileName);
+                    if (task.Result.CountCharacters != null)
+                        Console.WriteLine($"   Сount number of characters in the file - {task.Result.CountCharacters}");
+                    if (task.Result.CountCapltialLetter != null)
+                        Console.WriteLine($"   Count words which start with a Capital letter - {task.Result.CountCapltialLetter}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task<FileProcessingResult> ProcessingFile(string fn, IEnumerable<string> stropWords, bool countCharacters, bool countCapltialLetter)
+        {
+            int characters = 0;
+            int capltialLetter = 0;
+
+            try
+            {
+                using var reader = File.OpenText(fn);
+                var fileText = await reader.ReadToEndAsync();
+
+                Parallel.ForEach(fileText.Split(' ').Select(w => w.Trim()), word =>
+                {
+                    if (stropWords.Contains(word))
+                        return;
+
+                    if (countCharacters)
+                        characters += word.Length;
+
+                    if (countCapltialLetter && word[0] == char.ToUpper(word[0]))
+                    {
+                        System.Diagnostics.Debug.WriteLine(word);
+                        capltialLetter++;
+                    }
+                });
+
+                return new FileProcessingResult(fn, countCharacters ? characters : null, countCapltialLetter ? capltialLetter : null, null);
+            }
+            catch (Exception ex)
+            {
+                return new FileProcessingResult(fn, null, null, ex);
+            }
+        }
+    }
+}
